5b1e8297c4f41ec16be58c2b9b62aa0a0b163660
Jesulini <jesulini14@gmail.com>
2025-11-28 09:50:36 -0500

    descarga del ar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,16 +5,18 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDB_VlTE3mG2NlL5t5wzzOzEdyDvScETo4",
-    authDomain: "ar-taller-d0ee6.firebaseapp.com",
-    projectId: "ar-taller-d0ee6",
-    storageBucket: "ar-taller-d0ee6.appspot.com",
-    messagingSenderId: "183227498230",
-    appId: "1:183227498230:web:c456b5299c44c2fc48f6d7"
+  apiKey: "AIzaSyDHHn51TrMuQm3Jt8wD9t-EGXz52ycCv2I",
+  authDomain: "virtualwizzard-40f8b.firebaseapp.com",
+  projectId: "virtualwizzard-40f8b",
+  storageBucket: "virtualwizzard-40f8b.firebasestorage.app",
+  messagingSenderId: "50738706472",
+  appId: "1:50738706472:web:43100f7dc6e4b3c0bc5d1d",
+  measurementId: "G-98CXZKF761"
   },
-  supabaseUrl: "https://hleitdomwaeltpxrbjrb.supabase.co",
+ supabaseUrl: "https://hleitdomwaeltpxrbjrb.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsZWl0ZG9td2FlbHRweHJianJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM5NDEwNjAsImV4cCI6MjA3OTUxNzA2MH0.8PmSPEor-qnzcmumBQ_dlNxvujPJQ9MDXfc6NwL6SwU"
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
